c4521da8956191e50807554d7a3b5ab3843fa15a
SolitudePy <47316655+SolitudePy@users.noreply.github.com>
2025-06-13 15:53:56 +0300

    lsof error /dev/null

--- Collector/lf.sh
+++ Collector/lf.sh
@@ -422,7 +422,7 @@ generate_file_analysis_info() {
   find / -type f -not -path "/proc/*" -not -path "/sys/*" -amin -$((recent_read_files_threshold * 60)) -printf "%AY-%Am-%Ad %AH:%AM,%p\n" 2>/dev/null > "$FILE_ANALYSIS_DIR/recent_accessed_files.txt"
   find / -type f -executable -mmin -$((recent_modified_executables_threshold * 60)) -printf "%TY-%Tm-%Td %TH:%TM,%p\n" 2>/dev/null > "$FILE_ANALYSIS_DIR/recent_modified_executable_files.txt"
   find / -type f -executable -print0 2>/dev/null | xargs -0 sha256sum 2>/dev/null > "$FILE_ANALYSIS_DIR/executable_files_sha256.txt"
-  lsof > "$FILE_ANALYSIS_DIR/open_files.txt"
+  lsof > "$FILE_ANALYSIS_DIR/open_files.txt" 2>/dev/null
   create_file_if_output_not_empty "find / -type d -name '\.*'" "$FILE_ANALYSIS_DIR/hidden_directories.txt"
   
   # Generate bodyfile as part of file analysis
